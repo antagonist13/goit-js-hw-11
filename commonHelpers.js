da2cdd148835d364a71744f413c8b01f940018dd
antagonist13 <147069151+antagonist13@users.noreply.github.com>
2024-04-01 16:44:53 +0000

    Deploying to gh-pages from @ antagonist13/goit-js-hw-11@0639377298a8c83c2d5d79eba5f1cb8ecbc4f58b 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,15 +1,15 @@
-import"./assets/vendor-DcaNd_Y2.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();function a(s){const r="https://pixabay.com",n="/api/",o=new URLSearchParams({key:"43176233-1ec673b2575ffa542635b4c36",q:`${s}`,image_type:"photo",orientation:"horizontal",safesearch:!0}),e=`${r}${n}?${o}`;return fetch(e,{"Content-Type":"application/json"}).then(i=>i.json())}function l({largeImageURL:s,webformatURL:r,tags:n,likes:o,views:e,comments:t,downloads:i}){return`<li class="gallery-item">
-  <a class="gallery-link" href="${s}">
+import{i as l,S as p}from"./assets/vendor-BvkBopno.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();function u(o){const s="https://pixabay.com",r="/api/",i=new URLSearchParams({key:"43176233-1ec673b2575ffa542635b4c36",q:`${o}`,image_type:"photo",orientation:"horizontal",safesearch:!0}),e=`${s}${r}?${i}`;return fetch(e,{"Content-Type":"application/json"}).then(n=>n.json())}function d({largeImageURL:o,webformatURL:s,tags:r,likes:i,views:e,comments:t,downloads:n}){return`<li class="gallery-item">
+  <a class="gallery-link" href="${o}">
     <img
       class="gallery-image"
-      src="${r}"
-      alt="${n}"
+      src="${s}"
+      alt="${r}"
     />
   </a>
   <div class="description">
   <div class="img-info">
   <span class="description-text">Likes</span>
-  <p class="small-text">${o}</p>
+  <p class="small-text">${i}</p>
 </div>
 <div class="img-info">
   <span class="description-text">Views</span>
@@ -21,8 +21,8 @@ import"./assets/vendor-DcaNd_Y2.js";(function(){const r=document.createElement("
 </div>
 <div class="img-info">
   <span class="description-text">Downloads</span>
-  <p class="small-text">${i}</p>
+  <p class="small-text">${n}</p>
 </div>
 </div>
-</li>`}const p=document.querySelector(".gallery"),u=document.querySelector(".gallery-form"),c=document.querySelector(".search-window");u.addEventListener("submit",s=>{s.preventDefault(),d().then(r=>{const n=r.hits;p.innerHTML=m(n)}),c.value=""});function d(){return console.log(c.value),a(c.value)}function m(s){return s.map(l).join("")}
+</li>`}const m=document.querySelector(".gallery"),f=document.querySelector(".gallery-form"),c=document.querySelector(".search-window"),a=document.querySelector(".loader");f.addEventListener("submit",o=>{o.preventDefault(),a.style.display="block",g().then(s=>{const r=s.hits;s.hits==0&&l.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),m.innerHTML=y(r),new p(".gallery li a",{captionsData:"alt",captionsDelay:250}).refresh()}).then(s=>{a.style.display="none"}).catch(s=>console.log(s)),c.value=""});function g(){const o=u(c.value);return console.log(o),o}function y(o){return o.map(d).join("")}
 //# sourceMappingURL=commonHelpers.js.map
